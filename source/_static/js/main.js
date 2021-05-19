946c43785b88acd27ab381461ee286e7a49b30dd
ravindk89 <ravind@min.io>
2021-05-18 20:28:04 -0400

    SCSS fixups

--- source/_static/js/main.js
+++ source/_static/js/main.js
@@ -6,6 +6,23 @@ window.addEventListener("DOMContentLoaded", (event) => {
 			.appendChild(document.getElementById("table-of-contents"));
 	}
 
+   var l3 = document.getElementsByClassName("toctree-l3 current")
+   var l2 = document.getElementsByClassName("toctree-l2 current")
+   var l1 = document.getElementsByClassName("toctree-l1 current")
+
+   if (l3.length > 0) {
+      l3[0].classList.add("active");
+      l2[0].classList.add("active-parent");
+      l1[0].classList.add("active-parent");
+   }
+   else if (l2.length > 0 ) {
+      l2[0].classList.add("active");
+      l1[0].classList.add("active-parent");
+   }
+   else if (l1.length > 0 ) {
+      l1[0].classList.add("active");
+   }
+
 	// Toggle Sidebars
 	$('body').on('click', '[data-toggle]', function() {
 		var target = $(this).attr('data-toggle');
