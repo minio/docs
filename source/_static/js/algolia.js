211903d0174d970c593868cb1554c8e43b1e2ab9
Rushan <rushenn123@gmail.com>
2022-09-14 18:30:35 +0400

    Implement Algolia search

--- source/_static/js/algolia.js
+++
@@ -1,9 +0,0 @@
-docsearch({
-  apiKey: '0ba0d26da4d1483f96c03fe508304a64',
-  indexName: 'minio',
-  inputSelector: '#algolia input[type=text],
-  debug: false,
-  algoliaOptions: {
-    hitsPerPage: 8,
-  }
-});
