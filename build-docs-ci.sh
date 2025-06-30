132999c5cdb5a3f83ff391d3b5e12d68fcabd7d9
Ravind Kumar <ravind@min.io>
2025-06-30 11:44:58 -0400

    Fixing staging links

--- build-docs-ci.sh
+++ build-docs-ci.sh
@@ -6,33 +6,7 @@ export PATH=${PATH}:${HOME}/.local/bin
 export GITDIR=main
 
 
-make SYNC_SDK=TRUE linux
-make windows macos container k8s openshift eks aks gke
+make SYNC_SDK=TRUE mindocs
 
-mkdir -p minio/kubernetes/upstream
-cp -vr build/${GITDIR}/k8s/html/* ./minio/kubernetes/upstream/
-
-mkdir -p minio/kubernetes/eks
-cp -vr build/${GITDIR}/eks/html/* ./minio/kubernetes/eks/
-
-
-mkdir -p minio/kubernetes/gke
-cp -vr build/${GITDIR}/gke/html/* ./minio/kubernetes/gke/
-
-mkdir -p minio/kubernetes/aks
-cp -vr build/${GITDIR}/aks/html/* ./minio/kubernetes/aks/
-
-mkdir -p minio/kubernetes/openshift
-cp -vr build/${GITDIR}/openshift/html/* ./minio/kubernetes/openshift/
-
-mkdir -p minio/container
-cp -vr build/${GITDIR}/container/html/* ./minio/container/
-
-mkdir -p minio/linux
-cp -vr build/${GITDIR}/linux/html/* ./minio/linux/
-
-mkdir -p minio/macos
-cp -vr build/${GITDIR}/macos/html/* ./minio/macos/
-
-mkdir -p minio/windows
-cp -vr build/${GITDIR}/windows/html/* ./minio/windows/
+mkdir -p minio/
+cp -vr build/${GITDIR}/mindocs/html/* ./minio/
